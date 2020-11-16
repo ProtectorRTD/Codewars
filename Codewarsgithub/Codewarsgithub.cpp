1c15b0f3e39578393825afd64c70aafd5eedaa9b
Егор Олейник <egorka123pro@gmail.com>
2020-11-16 23:26:44 +0100

    6 Kyu - Find The Parity Outlier  language C

--- Codewarsgithub/Codewarsgithub.cpp
+++ Codewarsgithub/Codewarsgithub.cpp
@@ -5,6 +5,17 @@
 
 int main()
 {
-    
+    //int values[] = { 2, 4, 0, 100, 4, 11, 2602, 36 };
+	int d = 0;
+	for (int i = 0; i < count; i++)
+	{
+		if(values[i] % 2 == 0)d++;
+		if(values[i] % 2 != 0)d--;
+	}
+	for(int i = 0; i<count; i++)
+	{
+		if(d>0 && values[i] %2 != 0) return values[i];
+		if (d < 0 && values[i] % 2 == 0) return values[i];
+	}
 }
 
